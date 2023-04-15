c4af659c90cba92809598c30fa49d37260a9ee65
Jackline Njaria <jackienjaria@gmail.com>
2023-04-15 12:21:47 +0300

    Fourth Commit Working Bottom Nav

--- lib/bottom_navbar.dart
+++ lib/bottom_navbar.dart
@@ -1,1 +1,47 @@
-import 'package:flutter/material.dart';
+// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// import 'package:flutter/material.dart';
+// import 'dispay1.dart';
+// import 'storedetails.dart';
+// import 'accountsettings.dart';
+
+// class BottomNav extends StatefulWidget {
+//   const BottomNav({super.key, required int currentIndex});
+
+//   @override
+//   State<BottomNav> createState() => _BottomNavState();
+// }
+
+// class _BottomNavState extends State<BottomNav> {
+//   int pageIndex = 0;
+//   final screens = {
+//     const Center(
+//       child: Text('Home'),
+//     ),
+//     const Center(
+//       child: Text('Store Profile'),
+//     ),
+//     const Center(
+//       child: Text('Settings'),
+//     ),
+//   };
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return CurvedNavigationBar(
+//       currentIndex: pageIndex,
+//       backgroundColor: Colors.purple.shade200,
+//       color: Colors.deepPurple.shade400,
+//       animationDuration: const Duration(milliseconds: 300),
+//       items: const [
+//         //homepage Display Gesture detector
+//         Icon(Icons.home),
+
+//         // Account Details Display
+//         Icon(Icons.person),
+
+//         // Account Settings Display
+//         Icon(Icons.settings)
+//       ],
+//     );
+//   }
+// }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,68 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:lumo_app/accountsettings.dart';
+import 'package:lumo_app/storedetails.dart';
 import 'dispay1.dart';
-import 'storedetails.dart';
-import 'accountsettings.dart';
 
-class HomePage extends StatelessWidget {
+import 'bottom_navbar.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+
+  final screens = [
+    HomeIcons(),
+    AccountSettings(),
+    StoreDetails(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    final items = <Widget>[
+      Icon(Icons.home),
+      Icon(Icons.person),
+      Icon(Icons.settings),
+    ];
     return Scaffold(
-      backgroundColor: Colors.purple.shade200,
-      appBar: AppBar(
         backgroundColor: Colors.purple.shade200,
-        title: const Center(child: Text('LumoApp')),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.purple.shade200,
-        color: Colors.purple.shade100,
-        animationDuration: const Duration(milliseconds: 300),
-        items: [
-          //homepage Display Gesture detector
-          GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return HomeIcons();
-                    },
-                  ),
-                );
-              },
-              child: Icon(Icons.home)),
-
-          // Account Details Display
-          GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return StoreDetails();
-                    },
-                  ),
-                );
-              },
-              child: Icon(Icons.person)),
-
-          // Account Settings Display
-          GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return AccountSettings();
-                    },
-                  ),
-                );
-              },
-              child: Icon(Icons.settings))
-        ],
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.purple.shade200,
+          title: const Center(child: Text('LumoApp')),
+        ),
+        body: screens[index],
+        bottomNavigationBar: CurvedNavigationBar(
+          index: index,
+          backgroundColor: Colors.purple.shade200,
+          color: Colors.deepPurple.shade400,
+          animationDuration: const Duration(milliseconds: 300),
+          items: items,
+          onTap: (index) => setState(() {
+            this.index = index;
+          }),
+        ));
   }
 }
